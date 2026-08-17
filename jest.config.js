7f616a9a8d3a704fc9c9b41765f9a1381cd8fb74
optimisme <optimisme@gmail.com>
2026-08-17 16:58:15 +0200

    TASK-020: Proves Puppeteer configurades i Netega fitxers sense Chrome

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,12 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['**/tests/**/*.test.ts'],
-  moduleFileExtensions: ['ts', 'js'],
-  collectCoverage: false,
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.json',
+    },
+  },
 };

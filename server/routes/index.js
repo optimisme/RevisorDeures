38db013449c81f4b390a86a5ece844a71ecd5139
optimisme <optimisme@gmail.com>
2026-08-21 10:06:58 +0200

    .

--- server/routes/index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// Rutes API es carregaran dinàmicament des de index.js
-// Aquest fitxer és el punt d'entrada del router
-
-module.exports = router;
--- server/utils/response.js
+++
@@ -1,5 +0,0 @@
-function sendResponse(res, statusCode, body) {
-  return res.status(statusCode).json(body);
-}
-
-module.exports = { sendResponse };
